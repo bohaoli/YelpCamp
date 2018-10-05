e8bf4fcfae27c5c8c7cd6fcdf24babb0b0e9b379
Bohao Li <bohaoli@brandeis.edu>
2018-10-05 18:43:29 +0000

    Configure Passport.js. Added register routes and register template.

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express    = require("express"),
     app        = express(),
     bodyParser = require("body-parser"),
     mongoose   = require("mongoose"),
+    passport   = require("passport"),
+    LocalStrategy = require("passport-local"),
     Campground = require("./models/campground"),
     Comment    = require("./models/comment"),
+    User       = require("./models/user"),
     seedDB     = require("./seeds")
 
 mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true});
@@ -12,7 +15,17 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
-
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "this-is-a-secret-of-Bohao",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -93,6 +106,27 @@ app.post("/campgrounds/:id/comments", function(req, res){
         }
     });
 });
+
+//show register form
+app.get("/register", function(req, res){
+    res.render("register");
+})
+//handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
+})
+
+
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp server has started!");
 });
