97fdf50267b8ecb937248b4f3b38e33922e4281e
Bohao Li <bohaoli@brandeis.edu>
2018-10-05 21:07:29 +0000

    Added logout route. Added a function called isLoggedIn and used it as a middleware. This can prevent user from adding a comment if not logged in.

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.get("/campgrounds/:id", function(req, res){
     });
 });
 
-app.get("/campgrounds/:id/comments/new", function(req, res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
     //find campground by id
     Campground.findById(req.params.id, function(err, foundCampground){
         if(err){
@@ -87,7 +87,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
     Campground.findById(req.params.id, function(err, campground){
         if(err){
             console.log(err);
@@ -137,7 +137,18 @@ app.post("/login", passport.authenticate("local",
     }), function(req, res){
 });
 
+//logout route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+});
 
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp server has started!");
