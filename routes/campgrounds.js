fb738d4e05788dd5997dae08dabd58708483c915
Bohao Li <bohaoli@brandeis.edu>
2018-10-06 02:24:44 +0000

    Associate users with comments and save author's name to a comment automatically. Associate users with campgrounds and save username and id to newly created campground and add username under the description of the campground. Prevent an unauthenticated user from posting a campground.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -18,27 +18,33 @@ router.get("/", function(req, res){
 });
 
 //CREATE - add new campground to DB
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
     //get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
     var newCampground = {
         name: name, 
         image: image,
-        description: description
+        description: description,
+        author: author
     };
     Campground.create(newCampground, function(err, newSite){
         if(err){
             console.log(err);
         }else{
+            // console.log(newSite);
             res.redirect("/campgrounds");
         }
     });
 });
 
 //NEW - show form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
     res.render("campgrounds/new");
 });
 
@@ -55,5 +61,13 @@ router.get("/:id", function(req, res){
         }
     });
 });
+
+//middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
