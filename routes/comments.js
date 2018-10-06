31ef20e704b9430d735722d4b1699a64c954a3f5
Bohao Li <bohaoli@brandeis.edu>
2018-10-06 20:02:21 +0000

    Added Edit route, Update route and Destroy route for comments. Added Edit and Delete buttons for comments. Authorization comments. Only someone who owns the comment can edit and delete it, although people access the Edit Page through things like PostMan, they can not edit the comment. Hide the Edit and Delete button to those have no authorization to the comment.

--- routes/comments.js
+++ routes/comments.js
@@ -43,12 +43,70 @@ router.post("/", isLoggedIn, function(req, res){
     });
 });
 
+//EDIT COMMENT
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            res.redirect("back");
+        }else{
+           Comment.findById(req.params.comment_id, function(err, foundComment){
+               if(err){
+                   res.redirect("back");
+               }else{
+                   res.render("comments/edit", {comment: foundComment, campground: foundCampground});
+               }
+           });
+        }
+    });
+});
+//UPDATE COMMENT
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updateComment){
+        if(err){
+            res.redirect("back");
+        }else{
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+//COMMENT DESTROY ROUTE
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            res.redirect("back");
+        }else{
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
 //middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next){
+     //is user logged  in?
+    if(req.isAuthenticated()){
+       Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect("back");
+            }else{
+                // does user own the comment?
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                }else{
+                    res.redirect("back");
+                }
+            }
+        }); 
+    }else{
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
